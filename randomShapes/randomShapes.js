cfcb65fe0aa1c4fca382bb7c928d2c901f5a4601
Swapnil Tiwari <swaptiwari@outlook.com>
2022-01-04 15:05:49 +0530

    Update randomShapes.js

--- randomShapes/randomShapes.js
+++ randomShapes/randomShapes.js
@@ -14,7 +14,8 @@ class randCircle {
   }
 
   createCircle() {
-    stroke(255);
+    strokeWeight(10);
+    stroke(this.g, this.b, this.r);
     fill(this.r, this.g, this.b, this.alpha);
     circle(this.x, this.y, this.radius);
   }
@@ -34,7 +35,8 @@ class randRect {
   }
 
   createRect() {
-    stroke(255);
+    strokeWeight(10);
+    stroke(this.g, this.b, this.r);
     fill(this.r, this.g, this.b, this.alpha);
     rect(0, 0, this.w, this.h);
   }
@@ -46,17 +48,22 @@ class randRect {
 }
 
 class randTriangle {
-  constructor(_x1, _y1, _x2, _y2, _x3, _y3) {
+  constructor(_x1, _y1, _x2, _y2, _x3, _y3, _r, _g, _b, _alpha) {
     this.x1 = _x1;
     this.x2 = _x2;
     this.x3 = _x3;
     this.y1 = _y1;
     this.y2 = _y2;
     this.y3 = _y3;
+    this.r = _r;
+    this.g = _g;
+    this.b = _b;
+    this.alpha = _alpha;
   }
 
   createTriangle() {
-    stroke(255);
+    strokeWeight(10);
+    stroke(this.g, this.b, this.r);
     fill(this.r, this.g, this.b, this.alpha);
     triangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
   }
@@ -65,15 +72,63 @@ class randTriangle {
 function setup() {
   createCanvas(9075, 6201);
   angleMode(DEGREES);
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 0; i <= 25; i++) {
     randCircles[i] = new randCircle(
       random(0, width),
       random(0, height),
-      random(50, 100),
+      random(500, 1000),
       random(0, 255),
       random(0, 255),
       random(0, 255),
-      random(20, 50)
+      random(10, 50)
     );
   }
+  for (let i = 0; i <= 25; i++) {
+    randRects[i] = new randRect(
+      random(0, width),
+      random(0, height),
+      random(500, 1000),
+      random(500, 1000),
+      random(0, 255),
+      random(0, 255),
+      random(0, 255),
+      random(10, 50),
+      random(0, 90)
+    );
+  }
+
+  for (let i = 0; i <= 10; i++) {
+    randTriangles[i] = new randTriangle(
+      random(0, width),
+      random(0, height),
+      random(0, width),
+      random(0, height),
+      random(0, width),
+      random(0, height),
+      random(0, 255),
+      random(0, 255),
+      random(0, 255),
+      random(10, 50)
+    );
+  }
+}
+
+function draw() {
+  background(0);
+  push();
+  for (let rans of randCircles) {
+    rans.createCircle();
+  }
+  pop();
+  push();
+  for (let rans of randRects) {
+    rans.createRect();
+    rans.rotateRect();
+  }
+  pop();
+  push();
+  for (let rans of randTriangles) {
+    rans.createTriangle();
+  }
+  pop();
 }
